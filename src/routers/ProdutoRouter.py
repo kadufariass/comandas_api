74ae7ecfac90747b865d816308822e039159cc54
kadufariass <kaduturma687@gmail.com>
2026-03-09 19:27:12 -0300

    feat: ajustes nos routers conforme aula04

--- src/routers/ProdutoRouter.py
+++ src/routers/ProdutoRouter.py
@@ -5,21 +5,21 @@ router = APIRouter()
 
 # Criar as rotas/endpoints: GET, POST, PUT, DELETE
 @router.get("/produto/", tags=["Produto"], status_code=200)
-def get_produto():
+async def get_produto():
     return {"msg": "produto get todos executado"}
 
 @router.get("/produto/{id}", tags=["Produto"], status_code=200)
-def get_produto(id: int):
+async def get_produto(id: int):
     return {"msg": "produto get um executado"}
 
 @router.post("/produto/", tags=["Produto"], status_code=200)
-def post_produto():
+async def post_produto():
     return {"msg": "produto post executado"}
 
 @router.put("/produto/{id}", tags=["Produto"], status_code=200)
-def put_produto(id: int):
+async def put_produto(id: int):
     return {"msg": "produto put executado"}
 
 @router.delete("/produto/{id}", tags=["Produto"], status_code=200)
-def delete_produto(id: int):
+async def delete_produto(id: int):
     return {"msg": "produto delete executado", "id":id}
